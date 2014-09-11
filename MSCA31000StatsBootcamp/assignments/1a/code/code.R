854fc693b7e55e063caf5268597de875287e2071
naveen v <nvenkataraman1@babson.edu>
2014-09-11 13:13:50 -0500

    assignment 1 materials commit 	deleted:    MSCA31000StatsBootcamp/assignments/1a/Assignment1a_Venkataraman.pages/Data/pasted-image-22.png 	deleted:    MSCA31000StatsBootcamp/assignments/1a/Assignment1a_Venkataraman.pages/Data/pasted-image-25.png 	deleted:    MSCA31000StatsBootcamp/assignments/1a/Assignment1a_Venkataraman.pages/Data/pasted-image-small-23.png 	deleted:    MSCA31000StatsBootcamp/assignments/1a/Assignment1a_Venkataraman.pages/Data/pasted-image-small-26.png 	modified:   MSCA31000StatsBootcamp/assignments/1a/Assignment1a_Venkataraman.pages/Index.zip 	modified:   MSCA31000StatsBootcamp/assignments/1a/Assignment1a_Venkataraman.pages/Metadata/Properties.plist 	modified:   MSCA31000StatsBootcamp/assignments/1a/Assignment1a_Venkataraman.pages/preview-micro.jpg 	modified:   MSCA31000StatsBootcamp/assignments/1a/Assignment1a_Venkataraman.pages/preview-web.jpg 	modified:   MSCA31000StatsBootcamp/assignments/1a/Assignment1a_Venkataraman.pages/preview.jpg 	modified:   MSCA31000StatsBootcamp/assignments/1a/code/code.R 	MSCA31000StatsBootcamp/assignments/1a/Assignment1a_Venkataraman.pages/Data/pasted-image-31.png 	MSCA31000StatsBootcamp/assignments/1a/Assignment1a_Venkataraman.pages/Data/pasted-image-35.png 	MSCA31000StatsBootcamp/assignments/1a/Assignment1a_Venkataraman.pages/Data/pasted-image-38.png 	MSCA31000StatsBootcamp/assignments/1a/Assignment1a_Venkataraman.pages/Data/pasted-image-small-32.png 	MSCA31000StatsBootcamp/assignments/1a/Assignment1a_Venkataraman.pages/Data/pasted-image-small-36.png 	MSCA31000StatsBootcamp/assignments/1a/Assignment1a_Venkataraman.pages/Data/pasted-image-small-39.png 	MSCA31000StatsBootcamp/assignments/1a/Assignment1a_Venkataraman.pdf 	MSCA31000StatsBootcamp/book/chapters/ch2/Q3exercise.csv 	MSCA31000StatsBootcamp/book/chapters/ch2/graphing_distributions.pdf 	MSCA31000StatsBootcamp/book/chapters/ch3/ 	MSCA31000StatsBootcamp/book/chapters/ch4/

--- MSCA31000StatsBootcamp/assignments/1a/code/code.R
+++ MSCA31000StatsBootcamp/assignments/1a/code/code.R
@@ -1,16 +1,38 @@
 ## Assignment 1a
+setwd("~/gitrepos/UChicagoAnalytics/MSCA31000StatsBootcamp/book/chapters/ch2")
+require(ggplot2)
+require(ggplot2)
+
+## Ch2: Q3
+## box plot of chess player position test
+
+chessplayers <- read.csv("Q3exercise.csv", header = T)
+cp1 <- melt(chessplayers)
+
+chessplayersboxplot <- ggplot(cp1, 
+                               aes(x = variable, 
+                                   y = value, 
+                                   fill = variable)) + 
+                            stat_boxplot(geom ='errorbar') + 
+                            geom_boxplot() +
+                            guides(fill = FALSE) + 
+                            stat_summary(fun.y=mean, geom="point", shape=3, size=4) + 
+                            xlab("player type") + 
+                            ylab("num pieces")
+
+print(chessplayersboxplot)
+
 
 ## Ch 2: Q7
 
 ## data prep
-setwd("~/gitrepos/UChicagoAnalytics/MSCA31000StatsBootcamp/book/chapters/ch2")
 graphdata <- read.csv("Q7exercise.csv", header = T)
 names(graphdata) <- c("eyecolor","numstudents")
 graphdata$eyecolor <- factor(graphdata$eyecolor)
 
 ## a. pie graph
 ## Code adapted from: http://bit.ly/1ukp8Rs
-require(ggplot2)
+
 piechart <- ggplot(graphdata, 
                    aes(x = 1, 
                        y = numstudents, 
@@ -42,8 +64,9 @@ horizontalbargraph <- ggplot(graphdata,
                         ggtitle("Eye Color") + 
                         geom_bar(stat = "identity", color = "black") + 
                         coord_flip() + 
-                        guides(fill = FALSE) + 
-                        theme(axis.title=element_blank())
+                        guides(fill = FALSE)  + 
+    xlab("Eye Color") +
+    ylab("Num Students")
 
 print(horizontalbargraph)
 
@@ -53,10 +76,10 @@ verticalbargraph <- ggplot(graphdata,
                              aes(x = reorder(eyecolor,numstudents), 
                                  y = numstudents, 
                                  fill = eyecolor)) +
-    ggtitle("Eye Color") + 
     geom_bar(stat = "identity", color = "black") + 
     guides(fill = FALSE) + 
-    theme(axis.title=element_blank())
+    xlab("Eye Color") +
+    ylab("Num Students")
 
 print(verticalbargraph)
 
@@ -68,3 +91,31 @@ graphdata <- graphdata %>%
 
 graphdata
 
+## Ch 3
+
+## Q1
+
+data <- data.frame(num = c(1,1,2,2,2,2,3,3,4,5,6,7))
+
+ggplot(data, aes(x = num)) + 
+    geom_histogram(binwidth=1)
+
+ggplot(data, aes(x = num)) + 
+    geom_density()
+
+## Q3 - 9
+
+a <- c(1,2,3,4,5)
+b <- c(-1,2,3,5,6)
+c <- c(-100,5,5,5,100)
+d <- c(-1,2,2,6,6)
+
+e <- c(15.2,18.8,19.3,19.7,20.2,21.8,22.1,29.4)
+f <- c(15.2,18.8,19.3,19.7,20.2,21.8,21.2,29.4)
+
+g <- c(1,3,4,6,12)
+
+## Q11
+
+cp <- melt(chessplayers)
+ggplot(cp, aes(x = value, group = variable)) + geom_density()
